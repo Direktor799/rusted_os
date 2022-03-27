96a1ede9959980e1a148ab61829347eb7a8284d8
Direktor799 <priceclaptrap@hotmail.com>
2022-03-28 03:18:02 +0800

    ✨ feat: 为panic添加文件和行号描述

--- src/main.rs
+++ src/main.rs
@@ -22,18 +22,5 @@ pub extern "C" fn rust_main() -> ! {
     println!("Hello rusted_os!");
     interrupt::init();
     memory::init();
-    unsafe {
-        for _ in 0..2 {
-            let frame_0 = match memory::frame::FRAME_ALLOCATOR.borrow_mut().alloc() {
-                Option::Some(frame_tracker) => frame_tracker,
-                Option::None => panic!("None"),
-            };
-            let frame_1 = match memory::frame::FRAME_ALLOCATOR.borrow_mut().alloc() {
-                Option::Some(frame_tracker) => frame_tracker,
-                Option::None => panic!("None"),
-            };
-            println!("{:?} and {:?}", frame_0.address(), frame_1.address());
-        }
-    }
     panic!("Dummy as fuck");
 }
--- src/panic.rs
+++ src/panic.rs
@@ -4,7 +4,16 @@ use core::panic::PanicInfo;
 /// panic处理
 #[panic_handler]
 fn panic_handler(info: &PanicInfo) -> ! {
-    println!("\x1b[1;31mpanic: '{}'\x1b[0m", info.message().unwrap());
+    if let Some(location) = info.location() {
+        println!(
+            "\x1b[1;31mPanicked at {}:{} '{}'\x1b[0m",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
+    } else {
+        println!("\x1b[1;31mPanicked: '{}'\x1b[0m", info.message().unwrap());
+    }
     shutdown()
 }
 
