a2478167e540839bdb794a688d4f762776a6ec8d
Direktor799 <priceclaptrap@hotmail.com>
2022-04-20 23:11:15 +0800

    🐞 fix: 修复config错误

--- os/src/config.rs
+++ os/src/config.rs
@@ -1,12 +1,12 @@
 //! 系统设置
 
-/// 2M内核堆
-pub const KERNEL_HEAP_SIZE: usize = 0x20_0000;
+/// 8M内核堆
+pub const KERNEL_HEAP_SIZE: usize = 0x80_0000;
 /// 4K页大小
 pub const PAGE_SIZE: usize = 0x1000;
 pub const PAGE_SIZE_BITS: usize = 12;
 /// QEMU内存结束地址
-pub const MEMORY_END_ADDR: PhysAddr = PhysAddr(0x8800_0000);
+pub const MEMORY_END_ADDR: usize = 0x8800_0000;
 
 /// 第一级队列时间片
 pub const TASK_QUEUE_FCFS1_SLICE: usize = 4000;
