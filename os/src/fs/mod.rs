5881cb23315d7b53a56efdda6894b39f79a3d97a
qiao <1730735794@qq.com>
2022-05-17 08:40:44 +0800

    ✨ feat(fs/mod.fs的use部分和touch函数): touch函数增加返回值
    efs和inodehandler改为pub use

--- os/src/fs/mod.rs
+++ os/src/fs/mod.rs
@@ -11,15 +11,14 @@ pub const BLOCK_SZ: usize = 512;
 type DataBlock = [u8; BLOCK_SZ];
 
 use crate::drivers::BLOCK_DEVICE;
-use crate::memory::frame::user_buffer::UserBuffer;
 use crate::sync::mutex::Mutex;
 use alloc::sync::Arc;
 use alloc::vec;
 use alloc::vec::Vec;
 use block_cache::BlockCacheManager;
 use block_cache::BLOCK_CACHE_MANAGER;
-use efs::EasyFileSystem;
-use vfs::InodeHandler;
+pub use efs::EasyFileSystem;
+pub use vfs::InodeHandler;
 
 pub static mut ROOT_INODE: Option<Arc<InodeHandler>> = None;
 
@@ -90,10 +89,10 @@ pub fn mkdir_by_path(path: &str) {
     };
 }
 
-pub fn touch_by_path(path: &str) {
+pub fn touch_by_path(path: &str) -> Option<Arc<InodeHandler>>  {
     let (parent_path, target) = path.rsplit_once('/').expect("Invalid path");
     let parent_inode = find_inode_by_path(parent_path).expect("Invalid parent directory");
-    parent_inode.create(target, layout::InodeType::File);
+    parent_inode.create(target, layout::InodeType::File)
 }
 pub fn create_link_by_path(path: &str, real_file_path: &str) {
     let (parent_path, target) = path.rsplit_once('/').expect("Invalid path");
@@ -110,10 +109,10 @@ pub fn check_valid_by_path(path: &str) -> bool {
     find_inode_by_path(path).is_some()
 }
 
-pub trait File: Send + Sync {
-    fn read(&self, buf: UserBuffer) -> usize;
-    fn write(&self, buf: UserBuffer) -> usize;
-}
+// pub trait File: Send + Sync {
+//     fn read(&self, buf: UserBuffer) -> usize;
+//     fn write(&self, buf: UserBuffer) -> usize;
+// }
 
 pub fn init() {
     unsafe {
