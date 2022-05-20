304c155554f2b646f9ecfe4416ff9ff6e6954c63
Direktor799 <priceclaptrap@hotmail.com>
2022-05-20 15:52:18 +0800

    ✨ feat: 重构读写程序

--- user/src/bin/shell.rs
+++ user/src/bin/shell.rs
@@ -12,9 +12,9 @@ const CREATE: u32 = 1 << 9;
 const TRUNC: u32 = 1 << 10;
 use alloc::string::{String, ToString};
 use alloc::vec::Vec;
+use core::str;
 use user_lib::console::get_char;
 use user_lib::*;
-use core::str;
 
 #[no_mangle]
 fn main() -> i32 {
@@ -41,10 +41,9 @@ fn main() -> i32 {
                 match args[0] {
                     "cd" => cd(&mut cwd, &args),
                     "mkdir" => app_mkdir(&args),
-                    "touch" => app_touch(&args),
-                    "read" => read_test(&args),
+                    "cat" => app_cat(&args),
                     "write" => write_test(&args),
-                    _ => println!("{}: command not found1111", args[0]),
+                    _ => println!("{}: command not found", args[0]),
                 }
             }
             cur.clear();
@@ -91,45 +90,48 @@ fn app_mkdir(args: &Vec<&str>) {
     }
 }
 
-fn app_touch(args: &Vec<&str>) {
+fn app_cat(args: &Vec<&str>) {
     if args.len() == 1 {
-        println!("missing operand");
+        // TODO: non-blocking io
+        println!("not supported yet");
         return;
     }
     for target in &args[1..] {
-        let fd = open(target, CREATE);
-        match fd {
-            -1 => println!("cannot open file '{}'", target),
-            _ => println! {"fd:{}",fd},
+        let mut buffer = [0u8; 128];
+        let fd = open(target, RDONLY);
+        if fd == -1 {
+            println!("{}: No such file or directory", target);
+        }
+        loop {
+            let len = read(fd as usize, &mut buffer);
+            match len {
+                -1 => {
+                    println!("{}: Out of resources", target);
+                    break;
+                }
+                0 => {
+                    break;
+                }
+                _ => print!("{}", str::from_utf8(&buffer[0..len as usize]).unwrap()),
+            }
         }
     }
 }
-fn read_test(args: &Vec<&str>) {
-    if args.len() == 1 {
-        println!("missing operand");
-        return;
-    }
-    let target = args[1];
-    let mut buffer = [0u8; 128];
-    match read(target.parse::<usize>().unwrap(), &mut buffer) {
-        -1 => println!("cannot read file '{}'", target),
-        _ => {
-            let len = buffer.iter().position(|&v| v == 0).unwrap_or(buffer.len());
-            let buf_str = str::from_utf8(&buffer[0..len]).unwrap().to_string();
-            println!("read_mess:{}",buf_str);
-        }
-    }
-}
-fn write_test(args: &Vec<&str>) {
-    if args.len() == 2 {
-        println!("missing operand");
-        return;
-    }
-    let target = args[1];
-    let buf_str = args[2].clone().as_bytes();
 
-    match write(target.parse::<usize>().unwrap(), buf_str.clone()) {
-        -1 => println!("cannot write file '{}'", target.parse::<usize>().unwrap(),),
-        _ => {}
+fn write_test(args: &Vec<&str>) {
+    if args.len() <= 2 {
+        println!("missing operand");
+        return;
+    }
+    let target = args[1];
+    let buf_str = args[2].as_bytes();
+    let fd = open(target, WRONLY | CREATE);
+    if fd == -1 {
+        println!("{}: No such file or directory", target);
+    }
+    let len = write(fd as usize, buf_str);
+    match len {
+        -1 => println!("{}: Out of resources", target),
+        _ => println!("ok"),
     }
 }
