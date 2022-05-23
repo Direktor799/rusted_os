47afb3dbda2c92f9951d748a199c5724d3dc95ee
Direktor799 <priceclaptrap@hotmail.com>
2022-05-24 03:34:18 +0800

    🔧 build: 添加img打包流程

--- rebuild-and-run.sh
+++ rebuild-and-run.sh
@@ -1,6 +1,9 @@
 cd user
 make clean
 make build
+cd ../fs_tool
+cargo build
+./target/debug/fs_tool -s ../user/target/riscv64gc-unknown-none-elf/release/ -t ../os/ -b 8192
 cd ../os
 make clean
 make run
