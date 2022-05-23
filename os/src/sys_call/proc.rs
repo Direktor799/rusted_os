b3340c27a3b29c7a311d928e33844f64eec9bffc
PopChicken <jimmy.jino@hotmail.com>
2022-05-23 17:11:39 +0800

    ✨ feat(task): 添加exec支持

--- os/src/sys_call/proc.rs
+++ os/src/sys_call/proc.rs
@@ -1,5 +1,8 @@
 //! 进程相关系统调用子模块
+use alloc::vec::Vec;
+
 use crate::interrupt::timer::get_time_ms;
+use crate::memory::frame::page_table::get_user_string_in_kernel;
 use crate::task::{
     add_new_task, exit_current_and_run_next, get_current_process, suspend_current_and_run_next,
 };
@@ -37,4 +40,30 @@ pub fn sys_fork() -> isize {
     new.pid.0 as isize
 }
 
-// pub fn sys_exec(path: *const u8, mut args: *const usize) -> isize {}
+pub fn sys_exec(path: *const u8, mut args: *const usize) -> isize {
+    let current = get_current_process().unwrap();
+    let token = current.get_user_token();
+    let path = get_user_string_in_kernel(token, path);
+    // let mut args_vec: Vec<String> = Vec::new();
+    // loop {
+    //     let arg_str_ptr = *translated_ref(token, args);
+    //     if arg_str_ptr == 0 {
+    //         break;
+    //     }
+    //     args_vec.push(get_user_string_in_kernel(token, arg_str_ptr as *const u8));
+    //     unsafe {
+    //         args = args.add(1);
+    //     }
+    // }
+    // if let Some(app_inode) = open_file(path.as_str(), OpenFlags::RDONLY) {
+    //     let all_data = app_inode.read_all();
+    //     let process = get_current_process().unwrap();
+    //     let argc = args_vec.len();
+    //     process.exec(all_data.as_slice(), args_vec);
+    //     // return argc because cx.x[10] will be covered with it later
+    //     argc as isize
+    // } else {
+    //     -1
+    // }
+    -1
+}
