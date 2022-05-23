91c65c662b5019c15fa28b7c1da1f97556821d8e
PopChicken <jimmy.jino@hotmail.com>
2022-05-23 08:44:14 +0800

    🐞 fix(fork): 修复错误的逻辑

--- os/src/sys_call/proc.rs
+++ os/src/sys_call/proc.rs
@@ -24,15 +24,12 @@ pub fn sys_getpid() -> isize {
 
 pub fn sys_fork() -> isize {
     let current = get_current_process().unwrap();
-    let old_pid = current.pid.0;
     let pcb = current.fork();
-    let current = get_current_process();
-    if current.unwrap().pid.0 == old_pid {
-        pcb.pid.0 as isize
-    } else {
-        add_new_task(pcb);
-        0
-    }
+    let trap_cx = pcb.get_trap_cx();
+    trap_cx.x[10] = 0;
+    add_new_task(pcb.clone());
+    println!("hello world!");
+    pcb.pid.0 as isize
 }
 
 // pub fn sys_exec(path: *const u8, mut args: *const usize) -> isize {}
